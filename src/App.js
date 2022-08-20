6bfe94557661cb1052c6194dd19197f97b865a06
Amanuel <weditedi25@gmail.com>
2022-08-20 22:28:04 +0100

    removed lines

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">Portfolio</div>;
 }
 
 export default App;
