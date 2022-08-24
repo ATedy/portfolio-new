d39b9ede85a46af87ef607d41515fb14d6fc1a60
Amanuel <weditedi25@gmail.com>
2022-08-24 15:45:21 +0100

    text body styled, view button hover effect added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,15 +6,29 @@ const Home = () => {
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p>Hi, my name is</p>
-        <h1> Amanuel Tedros</h1>
-        <h2>I am a full stack develope.</h2>
-        <p>
+        <p className="text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6] ">
+          {" "}
+          Amanuel Tedros
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I am a full stack developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. At inventore
           rem aut magni odio nisi, officia eligendi, neque voluptatibus veniam
           beatae sint. Odit, at dicta. Laborum omnis excepturi inventore
           nostrum.
         </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              {" "}
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   );
