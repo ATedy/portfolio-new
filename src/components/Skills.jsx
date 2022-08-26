35418fdc7465341d4e413590dcc03505c9cd01c4
Amanuel <weditedi25@gmail.com>
2022-08-26 11:35:23 +0100

    Html icon added and styled in skill comp

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,7 +10,26 @@ import GitHub from "../assets/github.png";
 import Mongo from "../assets/mongo.png";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div name="skills" className="bg-[#0a192f] text-gray-300">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+            Experience
+          </p>
+          <p className="py-4">Technologies I have worked with</p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 ">
+          <div>
+            <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
+            <p>HTML</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
