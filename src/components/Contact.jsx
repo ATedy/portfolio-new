f94057948a79af2bfe9d18d649bd73ef91ac2894
Amanuel <weditedi25@gmail.com>
2022-08-26 14:30:25 +0100

    gitform url added and its posting succesfully

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,11 @@ const Contact = () => {
       name="contact"
       className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 "
     >
-      <form action="" className="flex flex-col max-w-[600px] w-full">
+      <form
+        method="POST"
+        action="https://getform.io/f/0e478101-2d12-47a8-896b-48d35524e83d"
+        className="flex flex-col max-w-[600px] w-full"
+      >
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
             Contact
@@ -15,6 +19,27 @@ const Contact = () => {
             Submit the form below or send me an email
           </p>
         </div>
+        <input
+          className="bg-[#ccd6f6] p-2"
+          type="text"
+          name="name"
+          placeholder="Name"
+        />
+        <input
+          className="my-4 p-2 bg-[#ccd6f6]"
+          type="email"
+          name="name"
+          placeholder="Email"
+        />
+        <textarea
+          className="bg-[#ccd6f6] p-2 "
+          name="message"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+        <button className="text-white border-2 hover:bg-pink-600 px-4 py-3 my-8 flex items-center ">
+          Let's Collaborate
+        </button>
       </form>
     </div>
   );
