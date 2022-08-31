90b10f8b3cc1ead59863ca78a6af616cb66bef8c
Amanuel <weditedi25@gmail.com>
2022-08-31 17:04:27 +0100

    work button is lonking

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,7 +15,7 @@ const Skills = () => {
   return (
     <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
       {/* Container */}
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-auto">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600">
             Skills
