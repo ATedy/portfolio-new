135fefeffe99399f8ec552ffe0fd63dae8493210
Amanuel <weditedi25@gmail.com>
2022-08-31 16:40:27 +0100

    CV link added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -125,7 +125,9 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300 "
-              href="/"
+              href="https://drive.google.com/file/d/1BATZBVxrxqIKzox-5yTRb0n9Ehavw7oH/view?usp=sharing"
+              target="_blank"
+              rel="noreferrer"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
